746969ec096785abe6ab43ef8e7424463a98f346
hustille <hus@design-d.de>
2023-08-16 14:28:50 +0200

    sampler cleanup

--- sampler/fooocus.py
+++ sampler/fooocus.py
@@ -4,9 +4,127 @@ https://github.com/lllyasviel/Fooocus
 """
 
 
-from comfy.samplers import *
-
+import torch
 import comfy.model_management
+import comfy.utils
+import math
+import numpy as np
+import latent_preview
+from comfy.sample import *
+from comfy.samplers import *
+import comfy.model_base
+
+
+def sdxl_encode_adm_patched(self, **kwargs):
+    clip_pooled = kwargs["pooled_output"]
+    width = kwargs.get("width", 768)
+    height = kwargs.get("height", 768)
+    crop_w = kwargs.get("crop_w", 0)
+    crop_h = kwargs.get("crop_h", 0)
+    target_width = kwargs.get("target_width", width)
+    target_height = kwargs.get("target_height", height)
+
+    if kwargs.get("prompt_type", "") == "negative":
+        width *= 0.8
+        height *= 0.8
+    elif kwargs.get("prompt_type", "") == "positive":
+        width *= 1.5
+        height *= 1.5
+
+    out = []
+    out.append(self.embedder(torch.Tensor([height])))
+    out.append(self.embedder(torch.Tensor([width])))
+    out.append(self.embedder(torch.Tensor([crop_h])))
+    out.append(self.embedder(torch.Tensor([crop_w])))
+    out.append(self.embedder(torch.Tensor([target_height])))
+    out.append(self.embedder(torch.Tensor([target_width])))
+    flat = torch.flatten(torch.cat(out))[None, ]
+    return torch.cat((clip_pooled.to(flat.device), flat), dim=1)
+
+
+def patch_negative_adm():
+    comfy.model_base.SDXL.encode_adm = sdxl_encode_adm_patched
+
+patch_negative_adm()
+
+
+
+@torch.no_grad()
+def ksampler_with_refiner(model, positive, negative, refiner, refiner_positive, refiner_negative, latent,
+                          seed=None, steps=30, refiner_switch_step=20, cfg=7.0, sampler_name='dpmpp_2m_sde_gpu',
+                          scheduler='karras', denoise=1.0, disable_noise=False, start_step=None, last_step=None,
+                          force_full_denoise=False, callback_function=None):
+    # SCHEDULERS = ["normal", "karras", "exponential", "simple", "ddim_uniform"]
+    # SAMPLERS = ["euler", "euler_ancestral", "heun", "dpm_2", "dpm_2_ancestral",
+    #             "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_sde", "dpmpp_sde_gpu",
+    #             "dpmpp_2m", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "ddim", "uni_pc", "uni_pc_bh2"]
+
+    seed = seed if isinstance(seed, int) else random.randint(1, 2 ** 64)
+
+    device = comfy.model_management.get_torch_device()
+    latent_image = latent["samples"]
+
+    if disable_noise:
+        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
+    else:
+        batch_inds = latent["batch_index"] if "batch_index" in latent else None
+        noise = comfy.sample.prepare_noise(latent_image, seed, batch_inds)
+
+    noise_mask = None
+    if "noise_mask" in latent:
+        noise_mask = latent["noise_mask"]
+
+    preview_format = "JPEG"
+    if preview_format not in ["JPEG", "PNG"]:
+        preview_format = "JPEG"
+
+    previewer = latent_preview.get_previewer(device, model.model.latent_format)
+
+    pbar = comfy.utils.ProgressBar(steps)
+    def callback(step, x0, x, total_steps):
+        preview_bytes = None
+        if previewer:
+            preview_bytes = previewer.decode_latent_to_preview_image(preview_format, x0)
+        pbar.update_absolute(step + 1, total_steps, preview_bytes)
+
+    sigmas = None
+    disable_pbar = False
+
+    if noise_mask is not None:
+        noise_mask = prepare_mask(noise_mask, noise.shape, device)
+
+    comfy.model_management.load_model_gpu(model)
+
+    noise = noise.to(device)
+    latent_image = latent_image.to(device)
+
+    positive_copy = broadcast_cond(positive, noise.shape[0], device)
+    negative_copy = broadcast_cond(negative, noise.shape[0], device)
+
+    refiner_positive_copy = broadcast_cond(refiner_positive, noise.shape[0], device)
+    refiner_negative_copy = broadcast_cond(refiner_negative, noise.shape[0], device)
+
+    models = load_additional_models(positive, negative, model.model_dtype())
+
+    sampler = KSamplerWithRefiner(model=model, refiner_model=refiner, steps=steps, device=device,
+                                  sampler=sampler_name, scheduler=scheduler,
+                                  denoise=denoise, model_options=model.model_options)
+
+    samples = sampler.sample(noise, positive_copy, negative_copy, refiner_positive=refiner_positive_copy,
+                             refiner_negative=refiner_negative_copy, refiner_switch_step=refiner_switch_step,
+                             cfg=cfg, latent_image=latent_image,
+                             start_step=start_step, last_step=last_step, force_full_denoise=force_full_denoise,
+                             denoise_mask=noise_mask, sigmas=sigmas, callback_function=callback, disable_pbar=disable_pbar,
+                             seed=seed)
+
+    samples = samples.cpu()
+
+    cleanup_additional_models(models)
+
+    out = latent.copy()
+    out["samples"] = samples
+
+    return (out, )
 
 
 class KSamplerWithRefiner:
