bf29ff193ae5863832ef13e069633f03d2dbbe02
hustille <hus@design-d.de>
2023-08-16 11:45:19 +0200

    updated combined sampler

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from .state import *
 from .style import *
 from .debug import *
 
+from .sampler.KSamplerWithRefiner import KSamplerWithRefiner
+
 NODE_CLASS_MAPPINGS = {
     "Fetch widget value": FetchNodeValue,
     "3way Prompt Styler": PromptStylerCSV3Way,
@@ -10,6 +12,7 @@ NODE_CLASS_MAPPINGS = {
     "Date Time Format": DateTimeFormat,
     "Batch State": BatchState,
     "Debug Extra": DebugExtra,
+    "KSampler With Refiner (Fooocus)": KSamplerWithRefiner,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,6 +22,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "DateTimeFormat": "Date Time Format",
     "BatchState": "Batch State",
     "DebugExtra": "Debug Extra",
+    "KSamplerWithRefiner" : "KSampler With Refiner (Fooocus)"
 }
 
 
