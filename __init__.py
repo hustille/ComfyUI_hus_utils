c164fe6ae5f4c81c732d940e54c3fcd618924b41
hustille <hus@design-d.de>
2023-08-16 16:39:14 +0200

    moved sampler to extra project

--- __init__.py
+++ __init__.py
@@ -3,8 +3,6 @@ from .state import *
 from .style import *
 from .debug import *
 
-from .sampler.KSamplerWithRefiner import KSamplerWithRefiner
-
 NODE_CLASS_MAPPINGS = {
     "Fetch widget value": FetchNodeValue,
     "3way Prompt Styler": PromptStylerCSV3Way,
@@ -12,7 +10,6 @@ NODE_CLASS_MAPPINGS = {
     "Date Time Format": DateTimeFormat,
     "Batch State": BatchState,
     "Debug Extra": DebugExtra,
-    "KSampler With Refiner (Fooocus)": KSamplerWithRefiner,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -22,7 +19,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "DateTimeFormat": "Date Time Format",
     "BatchState": "Batch State",
     "DebugExtra": "Debug Extra",
-    "KSamplerWithRefiner" : "KSampler With Refiner (Fooocus)"
 }
 
 
